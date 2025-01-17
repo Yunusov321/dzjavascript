db43b4d52ac58b4baf46badfc95f61d94cb255ca
Yunusov321 <diloromsaidova93@gmail.com>
2025-01-17 16:50:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function sayHello() {
+    alert("LightMode Funksiyasi ishldi");
+    document.body.style.backgroundColor = "#C4B5A5"
+    document.body.style.color = "black"
+}
+
+function sayHello2() {
+    alert("DarkMode Funksiyasi ishldi");
+    document.body.style.backgroundColor = "black"
+    document.body.style.color = "white"
+}
